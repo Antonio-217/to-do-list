6bb9d752db7dd910f61c03b1ff1c050d607530ef
Antonio Carlos Bueno <107446406+Antonio-217@users.noreply.github.com>
2024-03-17 21:22:24 -0300

    Implementação do mongoDB

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+ 
 require('./config/database');
 
 const app = express();
 app.use(express.json());
 
+//setando a pasta padrão do sistema e a pasta das views
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 /*Utilizando está rota, tens acesso a todos os métodos (GET, POST, PUT, DELETE)*/
 app.use('/checklists', checkListRouter);
-
-/*
-app.use(express.json());
-const log = (req, res, next) => {
-    console.log(req.body);
-    console.log(`Data: ${Date.now()}`);
-    next();
-}
-app.use(log);
-
-app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas</h1>');
-})
-
-app.get('/json', (req, res) => {
-    console.log(req.body);
-    res.json({
-        title: 'Tarefa 1',
-        done: true
-    })
-}) */
 
 app.listen(3000, () => {
     console.log('Servidor iniciado');
