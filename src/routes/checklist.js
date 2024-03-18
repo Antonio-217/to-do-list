791b5910ca02c412b51e7f6d90c87e0716909869
Antonio-217 <antoniocbuenas@gmail.com>
2024-03-17 21:17:12 -0300

    Implementação do mongoDB e EJS

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,34 +2,60 @@ const express = require('express');
 
 const router = express.Router();
 
+const Checklist = require('../models/checklist')
+
 //Pegar coisas no servidor
-router.get('/', (req, res) => {
-    console.log('Olá');
-    res.send();
+router.get('/', async (req, res) => {
+    try {
+        let checklist = await Checklist.find({});
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
 //Adicionar coisas no servidor
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body);
+router.post('/', async (req, res) => {
+    let {name} = req.body;
+
+    try {
+        let checklist = await Checklist.create({name})
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
 //Pegar coisas no servidor com parametro
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`ID: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
 //Atualizar coisas no servidor
-router.put('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`PUT ID: ${req.params.id}`);
+router.put('/:id', async (req, res) => {
+    try {
+        let {name} = req.body
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
 //Deletar coisas no servidor
-router.delete('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`DELETE ID: ${req.params.id}`);
+router.delete('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findByIdAndDelete(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        console.log(error)
+        res.status(422).json(error)
+    }
 })
 
 module.exports = router;
