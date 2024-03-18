d47d5998455b5ee9e16a810d5fbff3bebf328958
Antonio-217 <antoniocbuenas@gmail.com>
2024-03-17 21:31:50 -0300

    Implementação do EJS

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const path = require('path');
-
 const checkListRouter = require('./src/routes/checklist');
-const rootRouter = require('./src/routes/index');
- 
-require('./config/database');
 const rootRouter = require('./src/routes/index');
  
 require('./config/database');
@@ -14,9 +9,7 @@ require('./config/database');
 const app = express();
 app.use(express.json());
 
-//setando a pasta padrão do sistema e a pasta das views
-app.set('views', path.join(__dirname, 'src/views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', rootRouter);
 //setando a pasta padrão do sistema e a pasta das views
