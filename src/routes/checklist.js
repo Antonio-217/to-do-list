2a9cb67c7012baf783e69e3df66694db9682c3b7
Antonio-217 <antoniocbuenas@gmail.com>
2024-03-18 21:14:20 -0300

    Implementação das views (Visualização e criação)

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,27 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-const Checklist = require('../models/checklist')
+const Checklist = require('../models/checklist');
+const checklist = require('../models/checklist');
 
 //Pegar coisas no servidor
 router.get('/', async (req, res) => {
     try {
-        let checklist = await Checklist.find({});
-        res.status(200).json(checklist);
+        let checklists = await Checklist.find({});
+        res.status(200).render('checklists/index', {checklists: checklists})
     } catch (error) {
-        res.status(500).json(error);
+        res.status(500).render('pages/error', {error: 'Erro ao exibir as listas'});
+    }
+})
+
+router.get('/new', async (req, res) =>{
+    try {
+        let checklist = new Checklist();
+        res.status(200).render('checklists/new', {checklist: checklist});
+    } catch (error) {
+        res.status(500).render('pages/error', {error: 'Erro ao carregar o formulário'});
     }
 })
 
 //Adicionar coisas no servidor
 router.post('/', async (req, res) => {
-    let {name} = req.body;
-
+    let {name} = req.body.checklist;
+    let checklist = new Checklist({name});
     try {
-        let checklist = await Checklist.create({name})
-        res.status(200).json(checklist);
+        await checklist.save()
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error);
+        res.status(422).render('/checklists/new', {checklist: {...checklist, error}})
     }
 })
 
@@ -30,9 +40,9 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findById(req.params.id);
-        res.status(200).json(checklist);
+        res.status(200).render('checklists/show', {checklist: checklist})
     } catch (error) {
-        res.status(422).json(error);
+        res.status(422).render('pages/error', {error: 'Erro ao exibir as listas de tarefas'});
     }
 })
 
